a9589d4e0581e196c6238f0a6d2065fae9a8d6d6
1letter <1letter@gmx.de>
2026-06-06 21:16:35 +0200

    run plone.meta

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 
 from setuptools import setup
 
-
 long_description = "\n\n".join(
     [
         open("README.md").read(),
@@ -31,6 +30,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
