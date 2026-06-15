fd53011f8b249481b589606f3e08189cdf8fc3a2
1letter <1letter@gmx.de>
2026-06-15 18:59:57 +0200

    remove unused entry point in setup

--- setup.py
+++ setup.py
@@ -77,7 +77,5 @@ setup(
     entry_points="""
     [z3c.autoinclude.plugin]
     target = plone
-    [console_scripts]
-    update_locale = collective.outputfilters.tinymceaccordion.locales.update:update_locale
     """,
 )
